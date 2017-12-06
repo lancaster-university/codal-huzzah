49c1bdb9c1624c8108637b63cecca09a192335d6
James Devine <j.devine@lancaster.ac.uk>
2017-12-06 13:30:28 +0000

    Updated user_main to call int main().

--- source/user_main.cpp
+++ source/user_main.cpp
@@ -1,3 +1,16 @@
+extern "C"
+{
+#include "ets_sys.h"
+#include "os_type.h"
+#include "esp8266_sdk.h"
+#include "osapi.h"
+#include "gpio.h"
+#include "gpio16.h"
+#include "user_config.h"
+#include "eagle_soc.h"
+#include "user_interface.h"
+}
+
 extern int main();
 
 extern "C" void user_init(void)
