21f23e68116e2d0bb1048e35749cdaa9d2d84c45
James Devine <j.devine@lancaster.ac.uk>
2017-12-06 13:31:17 +0000

    updated main to blink and print hello world.

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,16 +1,30 @@
 extern "C"
 {
-    #include "ets_sys.h"
-    #include "osapi.h"
-    #include "gpio.h"
-    #include "os_type.h"
-    #include "user_config.h"
+#include "ets_sys.h"
+#include "os_type.h"
+#include "esp8266_sdk.h"
+#include "osapi.h"
+#include "gpio.h"
+#include "gpio16.h"
+#include "user_config.h"
+#include "eagle_soc.h"
+#include "user_interface.h"
 }
 
 int main()
 {
-    gpio_init();
-
-    GPIO_OUTPUT_SET (12, 1);
-    GPIO_OUTPUT_SET (12, 0);
+    uart_div_modify(0, UART_CLK_FREQ / 115200);
+    while(1)
+    {
+        os_printf("Hello");
+        system_soft_wdt_restart();
+        for(int p = 0; p < 17; p++)
+            GPIO_OUTPUT_SET(p,1);
+        os_delay_us(65535);
+        for(int p = 0; p < 17; p++)
+            GPIO_OUTPUT_SET(p,0);
+        system_soft_wdt_restart();
+        os_delay_us(65535);
+        os_printf(" World\r\n");
+    }
 }
